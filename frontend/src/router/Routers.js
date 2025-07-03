280b8fbf9b5449710b8e26250146e8474938db9e
baonqhe170954 <baonqhe170954@fpt.edu.vn>
2025-07-03 14:42:40 +0700

    admin user

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,12 +1,34 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/User/Home";
+import Admin from "../pages/Admin/components/Sidebar"
+import ManagerUser from "../pages/Admin/components/managerAccount"
+
+import AdminLayout from "../components/Layout/AdminLayout";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
+      {/* <Route path="/admin" element={<Admin />} /> */}
+      <Route
+        path="/admin"
+        element={
+          <AdminLayout>
+            <h2>Admin</h2> {/* Hoặc là một <h2>Dashboard</h2> tạm thời */}
+          </AdminLayout>
+        }
+      />
+      <Route
+        path="/admin/user"
+        element={
+          <AdminLayout>
+            <ManagerUser />
+          </AdminLayout>
+        }
+      />
+      {/* <Route path="/admin/user" element={<ManagerUser />} /> */}
     </Routes>
   );
 };
