47da9358a0d4bcf7602cb78449a08f8a51a0b0f3
baonqhe170954 <baonqhe170954@fpt.edu.vn>
2025-07-04 20:30:43 +0700

    update

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/User/Home";
+import Login from "../pages/User/Login";
+import Register from "../pages/User/Register";
+import ManagerUser from "../pages/Admin/components/managerAccount"
+
+import AdminLayout from "../components/Layout/AdminLayout";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/admin"
+        element={
+          <AdminLayout>
+            <h2>Admin</h2> {/* Hoặc là một <h2>Dashboard</h2> tạm thời */}
+          </AdminLayout>
+        }
+      />
+      <Route
+        path="/admin/user"
+        element={
+          <AdminLayout>
+            <ManagerUser />
+          </AdminLayout>
+        }
+      />
     </Routes>
   );
 };
